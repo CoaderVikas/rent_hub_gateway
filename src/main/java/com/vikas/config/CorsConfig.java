5312318e52c6520e753948059f75704dbf82e2d8
CoaderVikas <vikas.yadav83499@gmail.com>
2026-05-28 18:28:16 +0530

    added routing url

--- src/main/java/com/vikas/config/CorsConfig.java
+++ src/main/java/com/vikas/config/CorsConfig.java
@@ -26,7 +26,7 @@ public class CorsConfig implements WebFluxConfigurer  {
 		CorsConfiguration config = new CorsConfiguration();
 		// config.addAllowedOrigin("http://localhost:5173");
 		config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173",
-				"https://overappreciative-unilludedly-larae.ngrok-free.dev", "https://rent-hub-v1.vercel.app"));
+				"https://overappreciative-unilludedly-larae.ngrok-free.dev", "https://rent-hub-v1.vercel.app","https://rental-hyca.onrender.com"));
 		config.addAllowedMethod("*"); // allows GET, POST, etc.
 		config.addAllowedHeader("*");
 		config.setAllowCredentials(true);
