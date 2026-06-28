4ebec8ed953a65be5c89c898edd6ce9b79e03ed5
CoaderVikas <vikas.yadav83499@gmail.com>
2026-06-28 12:55:56 +0530

    added routing for google

--- src/main/java/com/vikas/filter/RouteValidator.java
+++ src/main/java/com/vikas/filter/RouteValidator.java
@@ -19,6 +19,7 @@ public class RouteValidator {
 
 	public static final List<String> openApiEndpoints = List.of(
 			"/api/v1/auth/login",
+			"/api/v1/auth/google" ,
 			"/api/v1/auth/register",
 			"/api/v1/auth/password-reset",
 			"/api/v1/auth/password-forgot",
